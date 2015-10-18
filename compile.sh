83ade3787f7b2a53471eb8cbfe699b349fffe21b
Genesis Palaganas <palaganas.genesis@gmail.com>
2015-10-18 23:16:54 +0800

    Update compile script to compile all files

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 cd  org/gcn/plinguacore/parser/input/plingua/
 java -cp "/home/genesis/Documents/Code/Utilities/Java/javacc/bin/lib/javacc.jar" javacc PlinguaJavaCcParser.jj
-cd /home/genesis/Documents/Thesis/P-Lingua/pLinguaCore4/
-javac org/gcn/plinguacore/**/*.java
+cd ../../../../../../
+javac $(find ./org/gcn/plinguacore/* | grep .java)
