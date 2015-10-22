7eaf78eaceec77b99e54c5d112276a04c7a720f1
Genesis Palaganas <palaganas.genesis@gmail.com>
2015-10-23 01:42:11 +0800

    Add module to write Synapses in bin file

--- org/gcn/plinguacore/parser/output/binary/SpikingBinaryOutputParser.java
+++ org/gcn/plinguacore/parser/output/binary/SpikingBinaryOutputParser.java
@@ -38,7 +38,7 @@ import org.gcn.plinguacore.util.psystem.spiking.SpikingPsystem;
 import org.gcn.plinguacore.util.psystem.spiking.membrane.SpikingEnvironment;
 import org.gcn.plinguacore.util.psystem.spiking.membrane.SpikingMembrane;
 import org.gcn.plinguacore.util.psystem.spiking.membrane.SpikingMembraneStructure;
-import org.gcn.plinguacore.util.psystem.spiking.membrane.SpikingMembraneStructure;
+import org.gcn.plinguacore.util.psystem.spiking.membrane.ArcInfo;
 
 //import org.gcn.plinguacore.simulator.cellLike.probabilistic.dcba.StaticMethods;
 //import org.gcn.plinguacore.simulator.cellLike.probabilistic.dcba.DCBAProbabilisticSimulator;
@@ -71,8 +71,6 @@ class SpikingBinaryOutputParser extends AbstractBinaryOutputParser{
 
     public void readRules(){
 
-        //TODO readRules
-        
         spikingRules.clear();
 
         for(IRule rule : getPsystem().getRules()){
@@ -85,7 +83,7 @@ class SpikingBinaryOutputParser extends AbstractBinaryOutputParser{
     }
 
     public void writeHeader() throws IOException{
-        //TODO write header according to format
+
         System.out.println(0xAF);
         System.out.println(0x12);
         System.out.println(0xFB);
@@ -93,23 +91,45 @@ class SpikingBinaryOutputParser extends AbstractBinaryOutputParser{
         getStream().write(0xAF);
         getStream().write(0x12);
         getStream().write(0xFB);
+
     }
 
     public void writeSubHeader() throws IOException{
-        SpikingMembraneStructure structure = (SpikingMembraneStructure)getPsystem().getMembraneStructure();
 
-        //String reserved Characters
-        int neuronCount = structure.getAllMembranes().size();
-        int ruleCount = spikingRules.size();
-
-        System.out.println("Number of Neurons (Including Environment): " + neuronCount);
-        System.out.println("Number of Rules: " + ruleCount);
-
-        getStream().write(neuronCount);
-        getStream().write(ruleCount);
-
+        writeReservedCharacters();
+        writeRuleCount();
+        writeNeuronCount();
         writeInitialConfiguration();
         
+    }
+
+    public void writeReservedCharacters(){
+
+        return;
+
+    }
+
+    public void writeRuleCount() throws IOException{
+
+        int ruleCount = spikingRules.size();
+
+        System.out.println("Number of Rules: " + ruleCount);
+
+        getStream().write(ruleCount);
+
+    }
+
+    public void writeNeuronCount() throws IOException{
+
+        SpikingMembraneStructure structure = (SpikingMembraneStructure)getPsystem().getMembraneStructure();
+
+        //The MembraneStructure considers the environment as another neuron
+        int neuronCount = structure.getAllMembranes().size() - 1;
+
+        System.out.println("Number of Neurons: " + neuronCount);
+
+        getStream().write(neuronCount);
+
     }
 
     public void writeInitialConfiguration() throws IOException{
@@ -142,18 +162,16 @@ class SpikingBinaryOutputParser extends AbstractBinaryOutputParser{
 
 
     public void writeSpikingRules() throws IOException{
-        //TODO write spiking rules according to format
 
         for(SpikingRuleBlock rule: spikingRules){
             System.out.println(rule.toString());
+
             if(rule.getRegExp().length() == 0)
                 getStream().writeByte(0);
             else
                 getStream().writeBytes(rule.getRegExp());
-            getStream().writeByte(rule.getConsumedSpikes());
-        }
 
-        for(SpikingRuleBlock rule: spikingRules){
+            getStream().writeByte(rule.getConsumedSpikes());
             getStream().writeByte(rule.getProducedSpikes());
             getStream().writeByte(rule.getDelay());
         }
@@ -163,31 +181,61 @@ class SpikingBinaryOutputParser extends AbstractBinaryOutputParser{
     public void writeLabels() throws IOException{
         //TODO write labels according to format
         return;
+
     }
 
-    public void writeNeurons() throws IOException{
-        //TODO write neurons according to format
-        return;
+    public void writeSynapses() throws IOException{
+
+        SpikingMembraneStructure structure = (SpikingMembraneStructure)getPsystem().getMembraneStructure();
+        int size = structure.getAllMembranes().size() - 1;
+        
+        int[][] matrix = structure.getMatrixRepresentation();
+
+        String binary = "";
+        
+        System.out.println("Matrix Representation");
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                binary += Integer.toString(matrix[i][j]);
+                
+                String out = Integer.toString(matrix[i][j]);
+                out += (j < size - 1)?",":"";
+                System.out.print(out);
+            }
+            System.out.println("");
+        }
+
+        int intRepr = Integer.parseInt(binary,2);
+
+        System.out.println("Integer Encoding: " + intRepr);
+
+        getStream().write(intRepr);
+
     }
 
-    public void writeFile() throws IOException{
-        readRules();
-        System.out.println("===HEADER===");
+    public void writeFile() throws IOException{ readRules(); System.out.println("===HEADER===");
+
         writeHeader();
         System.out.println("===SUB-HEADER===");
         writeSubHeader();
         System.out.println("===RULES===");
         writeSpikingRules();
         writeLabels();
-        writeNeurons();
+        System.out.println("===SYNAPSES===");
+        writeSynapses();
         System.out.println("===END===");
+        
     }
 
     public ByteOrder getByteOrder(){
+
         return ByteOrder.BIG_ENDIAN;
+
     }
 
     public byte getFileId(){
+
         return 0x31;
+
     }
 }
